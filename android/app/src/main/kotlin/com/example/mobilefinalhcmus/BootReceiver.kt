f85204d762c47f7c84d2e4521778de7f76db1016
caubexudua <volehoai070902@gmail.com>
2024-07-16 00:02:51 +0700

    fix error in gradle

--- android/app/src/main/kotlin/com/example/mobilefinalhcmus/BootReceiver.kt
+++ android/app/src/main/kotlin/com/example/mobilefinalhcmus/BootReceiver.kt
@@ -4,13 +4,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-
+import androidx.core.content.ContextCompat
 class BootReceiver:BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            val serviceIntent = Intent(context, SocketBackgroundServce::class.java)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                context.startForegroundService(serviceIntent)
+                val serviceIntent = Intent(context, SocketBackgroundServce::class.java)
+                serviceIntent.action = "START_SOCKET"
+                ContextCompat.startForegroundService(context, serviceIntent)
             }
         }
     }
